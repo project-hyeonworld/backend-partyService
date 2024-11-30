7faa92fc182bca4ae47bf6fcaed380ff4f3d53fd
hyeonwoody <hyeonwoody@gmail.com>
2024-11-30 15:37:49 +0900

    🎉INIT : Create party-service for MSA

---
+++ src/main/java/io/partyservice/PartyServiceApplication.java
@@ -0,0 +1,13 @@
+package io.partyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyServiceApplication.class, args);
+    }
+
+}
