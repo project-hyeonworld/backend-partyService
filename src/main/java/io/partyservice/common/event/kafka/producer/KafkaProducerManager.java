088acf45349f20fea2a573a8288de0f07ef03255
hyeonwoody <hyeonwoody@gmail.com>
2024-12-03 02:28:51 +0900

    🎨REFACTOR : make `KafkaProducerManager` generic for better type safety

--- src/main/java/io/partyservice/common/event/kafka/producer/KafkaProducerManager.java
+++ src/main/java/io/partyservice/common/event/kafka/producer/KafkaProducerManager.java
@@ -6,6 +6,6 @@ import io.partyservice.common.event.CustomEvent;
  * @author : hyeonwoody@gmail.com
  * @since : 24. 12. 1.
  */
-public interface KafkaProducerManager<E extends CustomEvent> {
-    KafkaProducerStrategy getProducer(E event);
+public interface KafkaProducerManager<E extends CustomEvent, KPS extends KafkaProducerStrategy> {
+    KPS getProducer(E event);
 }
